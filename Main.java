0950f711e28513a4e4cdd6ebc77ee46e586bab32
Jimmy Liu <jimmyjcl753@gmail.com>
2023-03-19 17:19:28 -0400

    Final edits

--- Main.java
+++ Main.java
@@ -13,22 +13,35 @@ public class Main {
         // use this Buffered Reader to read input
         BufferedReader br = new BufferedReader(new FileReader("input.txt"));
 
-        // TODO: Read the 11x11 char array as the Battlesnake board
-        // (an example of the input can be found in sample_input.txt)
+        /**
+         * TODO: Read the 11x11 char array as the Battlesnake board
+         * (an example of the input can be found in sample_input.txt)
+         * 
+         * "H" represents your snake's head
+         * "B" represents a segment of your snake's body
+         * "T" represents your snake's tail
+         * 
+         * "h" represents an enemy snake's head
+         * "b" represents a segment of an enemy snake's body
+         * "t" represents a an enemy snake's tail
+         * 
+         * "." represents empty space
+         * "f" represents a food pellet
+         */
+        for (int i = 0; i < 11; i++) {
+            String line = br.readLine().trim();
+            grid[i] = line.toCharArray();
+        }
 
-        // "H" represents your snake's head
-        // "B" represents a segment of your snake's body
-        // "T" represents your snake's tail
+        /**
+         * This is the remaining health of your snake (a number from 1 to 100)
+         */
+        int health_remaining = Integer.parseInt(br.readLine());
 
-        // "h" represents an enemy snake's head
-        // "b" represents a segment of an enemy snake's body
-        // "t" represents a an enemy snake's tail
-
-        // "." represents empty space
-        // "f" represents a food pellet
-
-        // TODO: output the move that YOUR snake will make (either "up", "down", "left",
-        // or "right")
+        /**
+         * TODO: output the move that YOUR snake will make (either "up", "down", "left",
+         * or "right")
+         */
         System.out.println("up"); // just an example. replace this with the move of your choice
     }
 }
--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 An **un**official Battlesnake template for **Java users**, written in Go.
 
+If you'd like to use repl.it to run your Battlesnake server, click the link below and press **"fork"** on the top right.
 [![Run on Replit](https://replit.com/@JimmyLiu3/starter-snake-java)](https://replit.com/@JimmyLiu3/starter-snake-java)
 
 
@@ -34,4 +35,6 @@ go run ./server verbose
 
 ## Next Steps
 
-**Note:** To play games and tournaments, you'll need to deploy your Battlesnake to a live web server such as [repl.it](https://repl.it)
+Try to make your Battlesnake smarter by avoiding other snakes and the edges of the board, going towards food, and suviving the arena as long as possible!
+
+**Note:** To play games and tournaments, you'll need to deploy your Battlesnake to a live web server. Here is a [repl.it template](https://replit.com/@JimmyLiu3/starter-snake-java) that you can fork to get your server live.
